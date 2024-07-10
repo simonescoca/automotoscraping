0915b4c4f0508a7c32c11059bc7d398eb0b36904
simonescoca <simone.scoca@icloud.com>
2024-07-10 13:37:02 +0200

    #

--- sort_json.py
+++ sort_json.py
@@ -1,27 +1,35 @@
 import os
 import json
 
+def main():
+    file = input("> inserisci il JSON da ordinare in formato nome-file.json: ")
 
-file = input("> inserisci il JSON da ordinare in formato nome-file.json: ")
+    if os.path.exists(f"{os.getcwd()}/{file}"):
+        key1 = input(f"> scegli la prima key su cui basare l'ordine di {file}: ")
+        key2 = input(f"> scegli la seconda key su cui basare l'ordine di {file} (lascia vuoto per bypassare): ")
 
-if  os.path.exists(f"{os.getcwd()}/{file}"):
-    key = input(f"> scegli la key su cui basare l'ordine di {file}: ")
+        # Carica il file JSON
+        with open(file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
 
-    # Carica il file JSON
-    with open(file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+        try:
+            if key2:
+                # Ordina i dati in base alle chiavi key1 e key2
+                data_sorted = sorted(data, key=lambda x: (x[key1].lower(), x[key2].lower()))
+            else:
+                # Ordina i dati solo in base alla chiave key1
+                data_sorted = sorted(data, key=lambda x: x[key1].lower())
+        except KeyError as e:
+            print(f"> la key {e} non è valida 𝘹")
+            return
 
-    try:
-        # Ordina i dati in base alla chiave 'name'
-        data_sorted = sorted(data, key=lambda x: x[key].lower())
-    except:
-        print("> la key non è valida")
+        # Salva i dati ordinati di nuovo nel file JSON
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(data_sorted, f, ensure_ascii=False, indent=4)
 
-    # Salva i dati ordinati di nuovo nel file JSON
-    with open(file, 'w', encoding='utf-8') as f:
-        json.dump(data_sorted, f, ensure_ascii=False, indent=4)
+        print(f"> {file} ordinato e salvato ✔")
+    else:
+        print(f"> {file} non esiste 𝘹")
 
-    print(f"> {file} ordinato e salvato ✔")
-
-else:
-    print(f"> {file} non esiste")
+if __name__ == "__main__":
+    main()
